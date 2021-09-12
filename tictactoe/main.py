b3d7bc68f3f58161f8f481af8380a129c41bc647
QueenSylvia <sylviawu2010@icloud.com>
2021-09-12 15:24:27 +1000

    program structure

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -15,3 +15,34 @@ while rules_input not in rules_option or len(rules_input) != 1:
     if rules_input == "Y":
         print("The rules are that you have to get three in a row and stop the other player from doing so before"
               "you do. The first one to do it wins. You will have alternating(swapping) turns.")
+
+
+def display_board():
+    # print out the board
+    print("    |    |   "
+          "----|----|---"
+          "----|----|---"
+          "    |    |   "
+          "             ")
+
+def game_init():
+    # initialise the game board
+    # we need to have a 3x3 array
+    # initialise both players -- someone take x, the other take o
+    pass
+
+
+def game_play(is_player):
+    # game play engine
+    pass
+
+
+def winning_condition():
+    # check the board to see if someone has won
+    return False
+
+
+game_init()
+while not winning_condition():
+    game_play()
+    display_board()
