8cd395a4581c27bd667db7ac5d49b7e72fa0288d
QueenSylvia <sylviawu2010@icloud.com>
2021-09-19 07:53:26 +1000

    Added no

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -13,8 +13,10 @@ while rules_input not in rules_option or len(rules_input) != 1:
     rules_input = input(rules).upper()
 
     if rules_input == "Y":
-        print("The rules are that you have to get three in a row and stop the other player from doing so before"
+        print("The rules are that you have to get three in a row and stop the other player from doing so before "
               "you do. The first one to do it wins. You will have alternating(swapping) turns.")
+    elif rules_input == "N":
+        pass
 
 
 def display_board():
@@ -24,6 +26,7 @@ def display_board():
           "----|----|---"
           "    |    |   "
           "             ")
+
 
 def game_init():
     # initialise the game board
